0d47806259b0ee95c0da01eab1fae0f947d0b17a
Mit <mit_rupang@yahoo.ca>
2018-05-29 16:35:59 -0400

    Many changes made including the filming page and func. of collapsible

---
+++ js/collapsible.js
@@ -0,0 +1,47 @@
+window.onload = function() {
+  var coll = document.getElementsByClassName("collapsible");
+  var currently_open = [];
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("open");
+      var content = this.nextElementSibling;
+      // clicking on an open collapsible
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+        index = currently_open.indexOf(this);
+        for (i = index + 1; i < currently_open.length; i++) {
+          currently_open[i].nextElementSibling.style.maxHeight = null;
+          currently_open[i].classList.toggle("open");
+        }
+        currently_open.splice(index, currently_open.length - index);
+      } else {
+          // closing already open sections
+          if (currently_open.length !== 0) {
+            // if one of the outer collapsibles is clicked
+            if (!this.classList.contains('inner')) {
+              for (i = 0; i < currently_open.length; i++) {
+                currently_open[i].nextElementSibling.style.maxHeight = null;
+                currently_open[i].classList.toggle("open");
+              }
+              currently_open = [];
+            } else if (currently_open[currently_open.length - 1].classList.contains('inner')) {
+              // if one of the inner collapsibles is clicked
+              var last_element = currently_open.pop();
+              last_element.nextElementSibling.style.maxHeight = null;
+              last_element.classList.toggle("open");
+            }
+          }
+          // opening the collapsible
+          content.style.maxHeight = content.scrollHeight + "px";
+          // for resizing the parent container when an inner collapsible opens
+          if (this.classList.contains('inner')) {
+            currently_open[0].nextElementSibling.style.maxHeight =
+            currently_open[0].nextElementSibling.scrollHeight + content.scrollHeight + "px";
+          }
+          currently_open.push(this);
+      }
+    });
+  }
+}
