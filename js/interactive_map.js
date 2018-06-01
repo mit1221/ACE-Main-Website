457e43f8e765a4fddb1881106b64026803d3dece
Mit <mit_rupang@yahoo.ca>
2018-06-01 08:09:14 -0400

    modified js code for map

--- js/interactive_map.js
+++ js/interactive_map.js
@@ -1,3 +1,13 @@
 window.onload = function() {
-  
+  var number_of_buttons = document.getElementById('map_navigation').children.length;
+  for (var i = 1; i <= number_of_buttons; i++) {
+    var current_buttons = document.getElementsByClassName(i);
+    for (var j = 0; j < current_buttons.length; j++) {
+      current_buttons[j].addEventListener('click', function() {
+        var image = document.createElement('IMG');
+        image.src = '../images/wedding_locations/' + i + '.jpg';
+        document.getElementById('map_canvas').appendChild(image);
+      })
+    }
+  }
 }
