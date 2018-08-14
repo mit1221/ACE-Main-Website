7ce4d1ac98241c313cc22229d1ff80a216a75e9c
Mit <mit_rupang@yahoo.ca>
2018-08-14 11:38:24 -0400

    changes

--- js/navbar.js
+++ js/navbar.js
@@ -12,12 +12,54 @@
     //   $('.nav-dropdown').hide();
     // });
     // Toggle open and close nav styles on click
-    $('.nav-mobile').click(function() {
-      $('.navigation nav ul').slideToggle(500);
+    // Hide Header on on scroll down
+    var didScroll;
+    var lastScrollTop = 0;
+    var navbarHeight = $('.navigation').outerHeight();
+    var delta = 5;
+
+    $(window).scroll(function(event){
+      didScroll = true;
     });
-    // Hamburger to X toggle
-    $('.nav-mobile').on('click', function() {
-      this.classList.toggle('active');
+
+    setInterval(function() {
+        if (didScroll) {
+          hasScrolled();
+          didScroll = false; // resetting variable
+        }
+    }, 250);
+
+    function hasScrolled() {
+      var st = $(this).scrollTop();
+
+      // Make sure they scroll more than delta
+      if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+      // If they scrolled down and are past the navbar, add class .nav-up.
+      // This is necessary so you never see what is "behind" the navbar.
+      if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('.navigation').removeClass('nav-down').addClass('nav-up');
+      } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+          $('.navigation').removeClass('nav-up').addClass('nav-down');
+        }
+      }
+
+      lastScrollTop = st;
+    }
+
+    $('.nav-mobile').click(function() {
+      $('.navigation nav ul').slideToggle(300);
+      // Hamburger to X toggle
+      if ($('#nav-toggle').hasClass('active')) {
+        document.body.style.overflow = 'auto';
+      } else {
+        document.body.style.overflow = 'hidden';
+      }
+
       $('#nav-toggle').toggleClass('active');
     });
   }); // end DOM ready
